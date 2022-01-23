8bb0510254bca446139543267a0e60d0e8cd4e4c
sidharthkanchiraju <60029736+sidharthkanchiraju@users.noreply.github.com>
2022-01-23 10:20:02 +0530

    LinearRegression(aesthetic)

--- Notez/js/noteApi.js
+++ Notez/js/noteApi.js
@@ -1,6 +1,6 @@
-export default class NotesAPI {
+export default class noteApi {
     static getAllNotes(){
-        const notes = JSON.parse(localStorage.getItem("notesapp-notes") || "[]");
+        const notes = JSON.parse(JSON.stringify(localStorage.getItem("notesapp-notes")) || "[]");
 
         return notes.sort((a,b) =>{
             return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
@@ -8,10 +8,10 @@ export default class NotesAPI {
 
     }
     static saveNote(noteToSave){
-        const notes = NotesAPI.getAllNotes();
+        const notes = noteAPI.getAllNotes();
 
         noteToSave.id = Math.floor(Math.random() * 1000000);
-        noteToSave.updated = new Date(). toISOString();  
+        noteToSave.updated = new Date().toISOString();  
         notes.push(noteToSave);
 
         localStorage.setItem("notesapp-notes", JSON.stringify(notes));
